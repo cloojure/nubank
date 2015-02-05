ef0d818635d5fa114ddfe5aa1bfbc3371c83ea36
Alan Thompson <cloojure@gmail.com>
2015-02-04 20:20:57 -0800

    misc

--- demo/src/demo/routes/home.clj
+++ demo/src/demo/routes/home.clj
@@ -2,6 +2,7 @@
   (:use cooljure.core)
   (:require [compojure.core     :refer :all]
             [demo.views.layout  :as layout]
+            [demo.graph         :as graph]
             [cooljure.parse     :as coolp]
   ))
 
@@ -15,16 +16,17 @@
     (println "received: (add-edge" n1 n2 ")" )
     (if (and (<= 0 n1 500) (<= 0 n2 500))
       (do 
-        (demo.graph/add-edge [n1 n2] )
+        (graph/add-edge [n1 n2] )
         (layout/common 
           [:h1 "Added Edge" 
             [:p (format "n1=%d" n1)]
             [:p (format "n2=%d" n2)]
           ] ))
       (do 
+        ; #todo add error response code
         (layout/common 
           [:h1 "Error - Invalid Node Values" 
-            [:p "Node values must be in the range [0 <= node < 500]"]
+            [:p "Node values must be in the range [0 <= node <= 500]"]
             [:p (format "n1=%d" n1)]
             [:p (format "n2=%d" n2)]
           ] ))
